e265948289c2e19668e7d5a9231c5144a28b20c0
jinliang <123456>
2018-11-13 15:35:59 +0800

    冗余代码删除

--- src/main/java/com/example/BootMqApplication.java
+++ src/main/java/com/example/BootMqApplication.java
@@ -7,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootMqApplication {
-
-
 	protected static Logger logger= LoggerFactory.getLogger(BootMqApplication.class);
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootMqApplication.class, args);
